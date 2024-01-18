509908f90a6c3181a295ce799775ebca27371335
Hamza Jadid <haljadid@proximie.com>
2024-01-18 16:53:09 +0200

    feat: push messages on receiving a message and wake the task

--- src/ws_stream.rs
+++ src/ws_stream.rs
@@ -2,11 +2,19 @@ use crate::safe_ws::SafeWebSocket;
 use futures_util::future::Either;
 use js_sys::wasm_bindgen::closure::Closure;
 use js_sys::wasm_bindgen::JsCast;
+use std::borrow::BorrowMut;
+use std::collections::VecDeque;
+use std::sync::Arc;
+use std::sync::Mutex;
+use std::task::Waker;
 use web_sys::BinaryType;
 use web_sys::Event;
+use web_sys::MessageEvent;
 
 pub struct WebSocketStream {
     inner: SafeWebSocket,
+    waker: Arc<Mutex<Option<Waker>>>,
+    queue: Arc<Mutex<VecDeque<String>>>,
 }
 
 impl WebSocketStream {
@@ -16,23 +24,23 @@ impl WebSocketStream {
 
         // Setting the onopen callback
         let (open_tx, open_rx) = futures_channel::oneshot::channel();
-        let on_open_callback = {
+        let on_open_cb = {
             let mut open_tx = Some(open_tx);
             Closure::wrap(Box::new(move |_e| {
                 open_tx.take().map(|open_tx| open_tx.send(()));
             }) as Box<dyn FnMut(Event)>)
         };
-        ws.set_onopen(Some(on_open_callback.as_ref().unchecked_ref()));
+        ws.set_onopen(Some(on_open_cb.as_ref().unchecked_ref()));
 
         // Settings the onerror callback
         let (err_tx, err_rx) = futures_channel::oneshot::channel();
-        let on_err_callback = {
+        let on_err_cb = {
             let mut err_tx = Some(err_tx);
             Closure::wrap(Box::new(move |_e| {
                 err_tx.take().map(|err_tx| err_tx.send(()));
             }) as Box<dyn FnMut(Event)>)
         };
-        ws.set_onerror(Some(on_err_callback.as_ref().unchecked_ref()));
+        ws.set_onerror(Some(on_err_cb.as_ref().unchecked_ref()));
 
         // Await futures
         // if open_tx resolves => success
@@ -45,6 +53,33 @@ impl WebSocketStream {
             Either::Right((_, _)) => return Err(crate::Error::ConnectionClosed),
         };
 
-        Ok(Self { inner: ws })
+        // Setting the onmessage callback to push the event and wake the task.
+        let waker = Arc::new(Mutex::new(Option::<Waker>::None));
+        let queue = Arc::new(Mutex::new(VecDeque::new()));
+
+        let on_message_cb = {
+            let waker = waker.clone();
+            let queue = queue.clone();
+
+            Closure::wrap(Box::new(move |_e: MessageEvent| {
+                queue.lock().unwrap().push_back(String::new());
+                if let Some(waker) = waker.lock().unwrap().borrow_mut().take() {
+                    waker.wake();
+                }
+            }) as Box<dyn FnMut(MessageEvent)>)
+        };
+        ws.set_onmessage(Some(on_message_cb.as_ref().unchecked_ref()));
+
+        Ok(Self {
+            inner: ws,
+            waker,
+            queue,
+        })
+    }
+}
+
+impl Drop for WebSocketStream {
+    fn drop(&mut self) {
+        self.inner.set_onmessage(None);
     }
 }
